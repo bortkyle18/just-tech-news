ea42cab92bee5ff4f1bd9191daa1d4fe5563c2ef
bortkyle18 <bortkyle18@gmail.com>
2022-07-05 15:48:37 -0400

    final

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,17 @@ const Sequelize = require('sequelize');
 // import username and password from env folder for security purposes
 require('dotenv').config({path: './env/.env'});
 
+let sequelize;
+
 // create connection to our database, pass in your MySQL information for username and password
-const sequelize = new Sequelize('just_tech_news_db', process.env.DB_USER, process.env.DB_PASSWORD, {
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+    } else {
+    sequelize = new Sequelize('just_tech_news_db', process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
-});
+    });
+}
 
 module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,73 +1,21 @@
-const { Model, DataTypes } = require('sequelize');
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
 
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 // turn on routes
 app.use(routes);
+
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
-
-// create our User model
-class User extends Model {}
-
-// define table columns and configuration
-User.init(
-    {
-        // define an id column
-        id: {
-            // use the special Sequelize DataTypes object provide what type of data it is
-            type: DataTypes.INTEGER,
-            // this is the equivalent of SQL's `NOT NULL` option
-            allowNull: false,
-            // instruct that this is the Primary Key
-            primaryKey: true,
-            // turn on auto increment
-            autoIncrement: true
-        },
-        // define a username column
-        username: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        // define an email column
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            // there cannot be any duplicate email values in this table
-            unique: true,
-            // if allowNull is set to false, we can run our data through validators before creating the table data
-            validate: {
-            isEmail: true
-            }
-        },
-        // define a password column
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-            // this means the password must be at least four characters long
-            len: [4]
-            }
-        }
-    },
-        {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'user'
-    }
-);
-
-
-module.exports = User;
